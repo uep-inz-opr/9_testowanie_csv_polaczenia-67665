31ba5c681f65295a9d6a874058a54ce29dc0b3aa
Yelyzaveta Borysenko <102366032+67665@users.noreply.github.com>
2022-05-29 21:14:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,12 @@
-import csv
-calls_sum_dict = dict()
 
-with open('phoneCalls.csv','r') as fin: 
-    # csv.DictReader uses first line in file for column headings by default
-    reader = csv.reader(fin, delimiter = ",") # comma is default delimiter
-    headers = next(reader)
-    for row in reader:
-      from_subscr = int(row[0])
-      if from_subscr not in calls_sum_dict:
-        calls_sum_dict[from_subscr] = 0
-      # value = int(row[3])
-      value =1
-      calls_sum_dict[from_subscr] += value
+if __name__ == "__main__":
+    pass
+
+import csv
 
 class MenadzerPolaczen:
   def __init__(self, filename):
-    self.filenaCme = filename
+    self.filename = filename
     self.data_dict = self.read_data()
 
   def read_data(self):
@@ -36,6 +27,3 @@ class MenadzerPolaczen:
 
 if __name__ == '__main__':
  print(MenadzerPolaczen(input()).pobierz_najczesciej_dzwoniacego())
-
-if __name__ == "__main__":
-    pass
